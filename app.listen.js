79c6f7f9b11bbe2feebc177e2816053b51e8229a
Little-Wolfie <kiefkostello@gmail.com>
2023-04-13 09:09:31 +0100

    Fix bug

--- app.listen.js
+++ app.listen.js
@@ -4,5 +4,5 @@ const { PORT = 9090 } = process.env;
 
 app.listen(PORT, err => {
 	if (err) console.error(err);
-	else console.log(`Server running on port: ${port}`);
+	else console.log(`Server running on port: ${PORT}`);
 });
